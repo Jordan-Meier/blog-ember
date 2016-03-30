5fa2f1b583d9a1cf5c761ae45148136a3c789a12
Jordan Meier and Marika Allely <jbirdmeier@gmail.com>
2016-03-30 10:23:47 -0700

    add content to posts.JSON file and create routes for index, contact and about

--- app/models/post.js
+++ app/models/post.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr(),
-  date: new Date(),
+  date: DS.attr('date', { defaultValue() { return new Date(); } }),
   category: DS.attr(),
   body: DS.attr(),
   image: DS.attr()
